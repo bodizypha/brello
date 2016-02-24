61baf25b5f4d54f05767f28dafd5d046b48bcff5
bodizypha <jed.davis.email@gmail.com>
2016-02-23 20:53:26 -0600

    initial commit

---
+++ database/migrations/2016_02_24_024439_create_tag_task_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_task', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tag_task');
+    }
+}
